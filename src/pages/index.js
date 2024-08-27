48ab3f3c2c0103f88af7ad49df5ba83591edc100
yumeekim <yumee0827@naver.com>
2024-08-27 17:31:23 +0900

    feat:breadcrum 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,25 @@
 import React, { useState, useRef } from "react";
+import { BreadCrumb } from "primereact/breadcrumb";
+import Link from "next/link";
 
 //components
 import { Card } from "primereact/card";
 
 export default function Main() {
+  const items = [
+    { label: "메뉴1" },
+    { label: "메뉴2" },
+    {
+      label: "메뉴3",
+      template: () => <Link href="/inputtext">InputText</Link>,
+    },
+  ];
+  const home = { icon: "pi pi-home", url: "https://primereact.org" };
   return (
     <>
+      <BreadCrumb model={items} home={home} />
       <div className="card">
-        <Card title="Simple Card">
-          <p className="m-0">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
-            sed consequuntur error repudiandae numquam deserunt quisquam
-            repellat libero asperiores earum nam nobis, culpa ratione quam
-            perferendis esse, cupiditate neque quas!
-          </p>
-        </Card>
+        <Card title="어린이집 찾기">ㄴㄴ</Card>
       </div>
     </>
   );
