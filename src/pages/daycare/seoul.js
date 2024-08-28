af6b4334cac54aed04a23cfa261fd91ab253a14c
yumeekim <yumee0827@naver.com>
2024-08-28 14:09:50 +0900

    feat:시군구 선택별 서울 어린이집 리스트 dataTable 렌더링

--- src/pages/daycare/seoul.js
+++ src/pages/daycare/seoul.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 //components
 import { Card } from "primereact/card";
 import { SelectButton } from "primereact/selectbutton";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 //data
 import location from "@/src/data/location";
 
@@ -18,6 +20,17 @@ export default function Main() {
     setCode(e.value);
   };
 
+  //데이터 형태 변환 함수
+  const transformData = (list) => {
+    return list.map((item) => ({
+      craddr: item.craddr[0],
+      crfax: item.crfax[0],
+      crhome: item.crhome[0],
+      crname: item.crname[0],
+      crtel: item.crtel[0],
+    }));
+  };
+
   const fetchList = async (code) => {
     try {
       const response = await fetch(
@@ -26,7 +39,10 @@ export default function Main() {
 
       if (response.ok) {
         const result = await response.json();
-        setList(result.response.item);
+        const data = result.response.item;
+        const formedData = transformData(data);
+        setList(formedData);
+        console.log(list);
       } else {
         console.log("응답 오류");
       }
@@ -36,20 +52,23 @@ export default function Main() {
   };
 
   useEffect(() => {
-    if (code) {
-      fetchList(code);
-    } else {
-      fetchList();
-    }
+    fetchList(code);
   }, [code]);
 
   return (
     <>
       <Card title="서울 어린이집 리스트">
         <SelectButton value={code} onChange={handleChange} options={options} />
-        {list.map((item, id) => {
+        {/* {list.map((item, id) => {
           return <p key={id}>{item.craddr[0]}</p>;
-        })}
+        })} */}
+        <DataTable value={list} tableStyle={{ minWidth: "50rem" }}>
+          <Column field="crname" header="이름"></Column>
+          <Column field="craddr" header="주소"></Column>
+          <Column field="crtel" header="전화번호"></Column>
+          <Column field="crfax" header="팩스"></Column>
+          <Column field="crhome" header="홈페이지"></Column>
+        </DataTable>
       </Card>
     </>
   );
