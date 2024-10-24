eda644b7c276fad487c524c10743024a040b3e28
yumeekim <yumee0827@naver.com>
2024-10-24 15:39:23 +0900

    index.js 내용채움, vercel 배포 에러 수정

---
+++ src/pages/youth/index.js
@@ -0,0 +1,15 @@
+//청소년
+//pages>youth>index.js
+import React, { useState, useEffect } from "react";
+//components
+import { Card } from "primereact/card";
+
+export default function Main() {
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        <Card title="청소년 메인" />
+      </div>
+    </>
+  );
+}
