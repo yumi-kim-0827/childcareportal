637ae0250c7669c1527666bd44b60a5308778b2b
yumeekim <yumee0827@naver.com>
2024-12-18 10:13:23 +0900

    구글 애널리틱스 스크립트 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,20 +4,38 @@ import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
 import "./globals.css";
 import { PrimeReactProvider } from "primereact/api";
+import Script from "next/script";
+
 //components
 import SideBar from "@/src/components/sidebar/SideBar";
 import AutoBreadcrumb from "@/src/components/breadcrumb/AutoBreadcrumb";
 
 export default function App({ Component, pageProps }) {
   return (
-    <PrimeReactProvider>
-      <div className="p-3 flex">
-        <SideBar />
-        <div className="flex flex-col gap-4 pl-[300px] mx-auto flex-1 max-medium:pl-[200px]">
-          <AutoBreadcrumb />
-          <Component {...pageProps} />
+    <>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-DD7033VBTZ`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-DD7033VBTZ');
+        `}
+      </Script>
+      <PrimeReactProvider>
+        <div className="p-3 flex">
+          <SideBar />
+          <div className="flex flex-col gap-4 pl-[300px] mx-auto flex-1 max-medium:pl-[200px]">
+            <AutoBreadcrumb />
+            <Component {...pageProps} />
+          </div>
         </div>
-      </div>
-    </PrimeReactProvider>
+      </PrimeReactProvider>
+    </>
   );
 }
